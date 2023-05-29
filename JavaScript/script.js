e2ee77199c2a97f5bbe5a88036818b7c2e8b87c7
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2023-05-29 14:25:34 -0300

    Alterações feedback

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,45 +1,59 @@
-let FORCA = document.getElementById("FOR_BASE");
-let DESTREZA = document.getElementById("DES_BASE");
-let CONSTITUICAO = document.getElementById("CON_BASE");
-let INTELIGENCIA = document.getElementById("INT_BASE");
-let SABEDORIA = document.getElementById("SAB_BASE");
-let CARISMA = document.getElementById("CAR_BASE");
-let TOTAL_PTS = document.getElementsByClassName("valorPontos")[0];
-let BOTAO_DADO = document.getElementById("dado")
-let CALC_BTN = document.getElementById("calcular");
-let RESETAR_BTN = document.getElementById("resetar");
+//#region Atributos
+// Elementos de atributos
+let forca = document.getElementById("forBase");
+let destreza = document.getElementById("desBase");
+let constituicao = document.getElementById("conBase");
+let inteligencia = document.getElementById("intBase");
+let sabedoria = document.getElementById("sabBase");
+let carisma = document.getElementById("carBase");
+//#endregion
 
+//#region D20 
+// Elemento números aleatórios após rodar o D20
+let forD20 = document.getElementById("forD20");
+let desD20 = document.getElementById("desD20");
+let conD20 = document.getElementById("conD20");
+let intD20 = document.getElementById("intD20");
+let sabD20 = document.getElementById("sabD20");
+let carD20 = document.getElementById("carD20");
+//#endregion
+
+//#region Modificadores
 // Marcadores de modificadores
-let for_mod = document.getElementById("for_mod");
-let des_mod = document.getElementById("des_mod");
-let con_mod = document.getElementById("con_mod");
-let int_mod = document.getElementById("int_mod");
-let sab_mod = document.getElementById("sab_mod");
-let car_mod = document.getElementById("car_mod");
+let forMod = document.getElementById("forMod");
+let desMod = document.getElementById("desMod");
+let conMod = document.getElementById("conMod");
+let intMod = document.getElementById("intMod");
+let sabMod = document.getElementById("sabMod");
+let carMod = document.getElementById("carMod");
+//#endregion
+
+//#region Total Elemento
+// Elementos Total da soma
+let totalForca = document.getElementById('forTotal');
+let totalDestreza = document.getElementById('desTotal');
+let totalConstituicao = document.getElementById('conTotal');
+let totalInteligencia = document.getElementById('intTotal');
+let totalSabedoria = document.getElementById('sabTotal');
+let totalCarisma = document.getElementById('carTotal');
+//#endregion
+
+//#region Botões
+//-Botões da página
+let botaoDado = document.getElementById("dado");
+let calcularBotao = document.getElementById("calcular");
+let resetarBotao = document.getElementById("resetar");
+//#endregion
+
+//#region Eventos
+calcularBotao.addEventListener('click', calcular);
+resetarBotao.addEventListener('click', recarregarPagina);
+botaoDado.addEventListener('click', camposD20);
+//#endregion
 
 
-CALC_BTN.addEventListener('click', calcular);
-RESETAR_BTN.addEventListener('click', recarregar_pagina);
-BOTAO_DADO.addEventListener('click', campos_d20);
-
-function calcular() {
-
-    let TOTAL_FORCA = document.getElementById('FOR_TOTAL');
-    let TOTAL_DESTREZA = document.getElementById('DES_TOTAL');
-    let TOTAL_CONSTITUICAO = document.getElementById('CON_TOTAL');
-    let TOTAL_INTELIGENCIA = document.getElementById('INT_TOTAL');
-    let TOTAL_SABEDORIA = document.getElementById('SAB_TOTAL');
-    let TOTAL_CARISMA = document.getElementById('CAR_TOTAL');
-
-    TOTAL_FORCA.value = Number(FORCA.value) + Number(for_mod.value);
-    TOTAL_DESTREZA.value = Number(DESTREZA.value) + Number(des_mod.value);
-    TOTAL_CONSTITUICAO.value = Number(CONSTITUICAO.value) + Number(con_mod.value);
-    TOTAL_INTELIGENCIA.value = Number(INTELIGENCIA.value) + Number(int_mod.value);
-    TOTAL_SABEDORIA.value = Number(SABEDORIA.value) + Number(sab_mod.value);
-    TOTAL_CARISMA.value = Number(CARISMA.value) + Number(car_mod.value);
-}
-
-function gerar_numero_aleatorio() {
+function gerarNumeroAleatorio()
+{
     let array_numeros_aleatorios = [];
 
     for (let i = 0; i < 6; i++) {
@@ -48,44 +62,28 @@ function gerar_numero_aleatorio() {
     return array_numeros_aleatorios;
 }
 
-function campos_d20() {
-    let for_d20 = document.getElementById("for_d20");
-    let des_d20 = document.getElementById("des_d20");
-    let con_d20 = document.getElementById("con_d20");
-    let int_d20 = document.getElementById("int_d20");
-    let sab_d20 = document.getElementById("sab_d20");
-    let car_d20 = document.getElementById("car_d20");
-
-    numeros_d20 = gerar_numero_aleatorio();
-
-    for_d20.value = numeros_d20[0];
-    des_d20.value = numeros_d20[1];
-    con_d20.value = numeros_d20[2];
-    int_d20.value = numeros_d20[3];
-    sab_d20.value = numeros_d20[4];
-    car_d20.value = numeros_d20[5];
-
+function camposD20()
+{
+    numeros_d20 = gerarNumeroAleatorio();
+    
+    forD20.value = numeros_d20[0];
+    desD20.value = numeros_d20[1];
+    conD20.value = numeros_d20[2];
+    intD20.value = numeros_d20[3];
+    sabD20.value = numeros_d20[4];
+    carD20.value = numeros_d20[5];
+    
     modificador(numeros_d20);
 }
 
-function recarregar_pagina() {
-    window.location.reload(true);
-}
-
-function modificador(numeros_aleatorios) {
-    let for_mod = document.getElementById("for_mod");
-    let des_mod = document.getElementById("des_mod");
-    let con_mod = document.getElementById("con_mod");
-    let int_mod = document.getElementById("int_mod");
-    let sab_mod = document.getElementById("sab_mod");
-    let car_mod = document.getElementById("car_mod");
+function modificador(numeros_aleatorios)
+{
     numeros_d20 = numeros_aleatorios;
     let mod = []
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++)
+    {
         valor = numeros_d20[i];
-        console.log(numeros_d20);
-        console.log(valor);
         if (valor > 15) {
             mod[i] = Math.round(1 + (valor / 1.2));
         } else if (valor > 7) {
@@ -94,10 +92,25 @@ function modificador(numeros_aleatorios) {
             mod[i] = Math.round(-1 + (valor / 1.2));
         }
     }
-    for_mod.value = mod[0];
-    des_mod.value = mod[1];
-    con_mod.value = mod[2];
-    int_mod.value = mod[3];
-    sab_mod.value = mod[4];
-    car_mod.value = mod[5];
+    forMod.value = mod[0];
+    desMod.value = mod[1];
+    conMod.value = mod[2];
+    intMod.value = mod[3];
+    sabMod.value = mod[4];
+    carMod.value = mod[5];
+}
+
+function calcular()
+{
+    totalForca.value = Number(forca.value) + Number(forMod.value);
+    totalDestreza.value = Number(destreza.value) + Number(desMod.value);
+    totalConstituicao.value = Number(constituicao.value) + Number(conMod.value);
+    totalInteligencia.value = Number(inteligencia.value) + Number(intMod.value);
+    totalSabedoria.value = Number(sabedoria.value) + Number(sabMod.value);
+    totalCarisma.value = Number(carisma.value) + Number(carMod.value);
+}
+
+function recarregarPagina()
+{
+    window.location.reload(true);
 }
